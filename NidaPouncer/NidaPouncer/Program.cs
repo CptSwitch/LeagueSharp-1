0f623f2274e43067d5f775845b1366a8bf684171
DZ191 <darkprince97@live.it>
2014-09-13 19:29:12 +0200

    Test

--- NidaPouncer/NidaPouncer/Program.cs
+++ NidaPouncer/NidaPouncer/Program.cs
@@ -76,18 +76,20 @@ namespace NidaPouncer
             {
                 foreach (KeyValuePair<Vector3,Vector3> entry in positions)
                 {
-                    Game.PrintChat((player.Distance(entry.Key) <= 1500f && player.Distance(entry.Value) <= 1500f).ToString());
-                    
                         Drawing.DrawCircle(entry.Key, 50f, Color.GreenYellow);
                         Drawing.DrawCircle(entry.Value, 50f, Color.GreenYellow);
                     
                 }
+                Drawing.DrawCircle(new Vector3(11590.95f, 4656.26f, 0f), 80f, System.Drawing.Color.FromArgb(255, 255, 255, 255));
+
+
             }
         }
 
 
         public static void fillPositions()
         {
+            
             Vector3 pos0 = new Vector3(6393.7299804688f, -63.87451171875f, 8341.7451171875f);
             Vector3 pos1 = new Vector3(6612.1625976563f, 56.018413543701f, 8574.7412109375f);
             positions.Add(pos0, pos1);
