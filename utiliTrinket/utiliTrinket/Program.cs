3e8fc7887a904e9e8d4ba9f7e550f53c8b47c9a7
DZ191 <darkprince97@live.it>
2014-09-20 16:49:07 +0200

    Debug2

--- utiliTrinket/utiliTrinket/Program.cs
+++ utiliTrinket/utiliTrinket/Program.cs
@@ -55,7 +55,7 @@ namespace utiliTrinket
             Obj_AI_Hero player1 = (Obj_AI_Hero)player;
  	        if(player.IsDead || Utility.InShopRange())
             {
-                Game.PrintChat(hasItem(YellowW).ToString);
+                Game.PrintChat(hasItem(YellowW).ToString());
                 if(GetTimer()<1 && !hasItem(YellowW) && isEn("ward"))
                 {
                     Packet.C2S.BuyItem.Encoded(new Packet.C2S.BuyItem.Struct(YellowW, ObjectManager.Player.NetworkId)).Send();
