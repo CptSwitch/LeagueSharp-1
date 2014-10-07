d26c697c1a44c7a44874a0ab1baee830604b6786
DZ191 <darkprince97@live.it>
2014-10-07 17:41:40 +0200

    Removed

--- MinionHPBar/MinionHPBar/Program.cs
+++ MinionHPBar/MinionHPBar/Program.cs
@@ -65,7 +65,7 @@ namespace DZDraven
                     {
                         if(i!=0 || i!=BTD-1)
                         {
-                            Drawing.DrawLine(new Vector2(HPBarPos.X + 45.1f + (float)(barDistanceBetween) * i, HPBarPos.Y + 18), new Vector2(HPBarPos.X + 45.1f + ((float)(barDistanceBetween) * i), HPBarPos.Y + 23), 1f, (minion.Health<=DamageCalculator.Calculate((Obj_AI_Hero)player, minion))?Color.Lime:Color.Black);
+                            Drawing.DrawLine(new Vector2(HPBarPos.X + 45.1f + (float)(barDistanceBetween) * i, HPBarPos.Y + 18), new Vector2(HPBarPos.X + 45.1f + ((float)(barDistanceBetween) * i), HPBarPos.Y + 23), 1f, (minion.Health<=Damage.GetAutoAttackDamage(player,minion,true)?Color.Lime:Color.Black);
                         }
                     }
                 }
@@ -74,79 +74,6 @@ namespace DZDraven
         static bool isEn(String opt)
         {
             return menu.Item(opt).GetValue<bool>();
-        }
-        //Creditz to Lizzaran <3
-        internal class DamageCalculator
-        {
-            public static double Calculate(Obj_AI_Hero player, Obj_AI_Base target)
-            {
-                //no % armor/magic penetration calculation
-                //can't find magic resist??
-                //no masteries yet
-                double targetArmor = target.Armor + target.FlatArmorMod;
-                double targetSpellbock = target.SpellBlock + target.FlatSpellBlockMod;
-                double attackDamage = player.BaseAttackDamage + player.FlatPhysicalDamageMod;
-                //Game.PrintChat(attackDamage.ToString());
-                double magicDamage = player.BaseAbilityDamage + player.FlatMagicDamageMod;
-
-                targetArmor -= player.FlatArmorPenetrationMod;
-                targetSpellbock -= player.FlatMagicPenetrationMod;
-
-                double totalDamage = attackDamage * (100 / (100 + targetArmor));
-
-                if (HasMasteryArcaneBlade())
-                {
-                    totalDamage += (magicDamage * 0.05) * (100 / (100 + targetSpellbock));
-                }
-                if (HasMasteryDoubleEdgedSword())
-                {
-                    if (player.AttackRange > 250)
-                    {
-                        totalDamage *= 1.02;
-                    }
-                    else
-                    {
-                        totalDamage *= 1.015;
-                    }
-                }
-                if (HasMasteryHavoc())
-                {
-                    totalDamage *= 1.03;
-                }
-                if (target.IsMinion & HasMasteryButcher())
-                {
-                    totalDamage += 2;
-                }
-                return totalDamage;
-            }
-
-            public static bool HasMasteryDoubleEdgedSword()
-            {
-                return
-                    ObjectManager.Player.Masteries.Where(mastery => mastery.Page == MasteryPage.Offense)
-                        .Any(mastery => mastery.Id == 65 && mastery.Points == 1);
-            }
-
-            public static bool HasMasteryButcher()
-            {
-                return
-                    ObjectManager.Player.Masteries.Where(mastery => mastery.Page == MasteryPage.Offense)
-                        .Any(mastery => mastery.Id == 68 && mastery.Points == 1);
-            }
-
-            public static bool HasMasteryArcaneBlade()
-            {
-                return
-                    ObjectManager.Player.Masteries.Where(mastery => mastery.Page == MasteryPage.Offense)
-                        .Any(mastery => mastery.Id == 132 && mastery.Points == 1);
-            }
-
-            public static bool HasMasteryHavoc()
-            {
-                return
-                    ObjectManager.Player.Masteries.Where(mastery => mastery.Page == MasteryPage.Offense)
-                        .Any(mastery => mastery.Id == 146 && mastery.Points == 1);
-            }
         }
     }
 }
