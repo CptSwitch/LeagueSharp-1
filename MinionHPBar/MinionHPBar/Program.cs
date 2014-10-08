7978a3a5d68d2ddee9ab596087c15890621ed8ba
DZ191 <darkprince97@live.it>
2014-10-08 15:34:15 +0200

    Update

--- MinionHPBar/MinionHPBar/Program.cs
+++ MinionHPBar/MinionHPBar/Program.cs
@@ -4,7 +4,6 @@ using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
 using Color = System.Drawing.Color;
-
 namespace MinionHPBar
 {
 
@@ -24,13 +23,16 @@ namespace MinionHPBar
             Menu.AddSubMenu(new Menu("Drawing", "Drawing"));
 
             //Drawings Menu
-            Menu.SubMenu("Drawing").AddItem(new MenuItem("DrawLines", "Draw Bars").SetValue(true));
+            Menu.SubMenu("Drawing").AddItem(new MenuItem("DrawLines", "Draw Bars").SetValue(new Circle(true,Color.Black)));
+            Menu.SubMenu("Drawing").AddItem(new MenuItem("LastHitH", "LastHit Helper").SetValue(new Circle(true,Color.Lime)));
+            Menu.SubMenu("Drawing").AddItem(new MenuItem("Thick", "Thickness").SetValue(new Slider(2,1,3)));
+            //Range Menu
             Menu.AddSubMenu(new Menu("Ranges", "Range"));
             Menu.SubMenu("Range").AddItem(new MenuItem("DrRange", "Draw Range").SetValue(new Slider(850,450,1200)));
             Menu.AddToMainMenu();
 
             //Credits
-            Game.PrintChat("MinionHPBar by DZ191. Damage Calculations by Lizzaran.");
+            Game.PrintChat("MinionHPBars by DZ191");
             
             //Events
             Drawing.OnDraw += Drawing_OnDraw;
@@ -38,9 +40,8 @@ namespace MinionHPBar
 
         private static void Drawing_OnDraw(EventArgs args)
         {
-            if (!Menu.Item("DrawLines").GetValue<bool>())
+            if (!Menu.Item("DrawLines").GetValue<Circle>().Active)
                 return;
-
             var minionList = MinionManager.GetMinions(Player.Position, Menu.Item("DrRange").GetValue<Slider>().Value, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.MaxHealth);
             foreach (var minion in minionList.Where(minion => minion.IsValidTarget(Menu.Item("DrRange").GetValue<Slider>().Value)))
             {
@@ -58,11 +59,12 @@ namespace MinionHPBar
                     {
                         Drawing.DrawLine(
                             new Vector2(hpBarPosition.X + 45 + (float) (barDistance)*i, hpBarPosition.Y + 18),
-                            new Vector2(hpBarPosition.X + 45 + ((float) (barDistance)*i), hpBarPosition.Y + 23), 1f,
-                            (minion.Health <= Player.GetAutoAttackDamage(minion, true) ? Color.Lime : Color.Black));
+                            new Vector2(hpBarPosition.X + 45 + ((float) (barDistance)*i), hpBarPosition.Y + 23), Menu.Item("Thick").GetValue<Slider>().Value,
+                            ((minion.Health <= Player.GetAutoAttackDamage(minion, true) && Menu.Item("LastHitH").GetValue<Circle>().Active) ? Menu.Item("LastHitH").GetValue<Circle>().Color : Menu.Item("DrawLines").GetValue<Circle>().Color));
                     }
                 }
             }
         }
+        
     }
 }
